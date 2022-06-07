92f0dfb57153d9a3246326f7d357dc1e662de5bb
AlvinImbua <bulimua@gmail.com>
2022-06-07 13:06:08 +0300

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    for a in my_string:
+        if a in "Cc":
+            my_string = my_string.split(a)
+            my_string = "".join(my_string)
+    return 
